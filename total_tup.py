8fb2b18b96507bad3b5337ace891d04c3e8f5329
Partul Maan <41269832+pmaan08@users.noreply.github.com>
2020-07-30 23:36:38 +0530

    Update total_tup.py

--- total_tup.py
+++ total_tup.py
@@ -1,27 +1,27 @@
 #proceesing for complete data set
-# Initialize reader object: urb_pop_reader
+# Initialize reader object
 urb_pop_reader = pd.read_csv('ind_pop_data.csv', chunksize=1000)
 
-# Initialize empty DataFrame: data
+# Initialize empty DataFrame
 data = pd.DataFrame()
 
 # Iterate over each DataFrame chunk
 for df_urb_pop in urb_pop_reader:
 
-    # Check out specific country: df_pop_ceb
+    # Check out specific country
     df_pop_ceb = df_urb_pop[df_urb_pop['CountryCode'] == 'CEB']
 
-    # Zip DataFrame columns of interest: pops
+    # Zip DataFrame columns of interest
     pops = zip(df_pop_ceb['Total Population'],
                 df_pop_ceb['Urban population (% of total)'])
 
-    # Turn zip object into list: pops_list
+    # Turn zip object into list
     pops_list = list(pops)
 
     # Use list comprehension to create new DataFrame column 'Total Urban Population'
     df_pop_ceb['Total Urban Population'] = [int(tup[0] * tup[1] * 0.01) for tup in pops_list]
     
-    # Append DataFrame chunk to data: data
+    # Append DataFrame chunk to data
     data = data.append(df_pop_ceb)
 
 # Plot urban population data
